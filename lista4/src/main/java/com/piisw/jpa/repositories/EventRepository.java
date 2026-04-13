9c039eab3512c9d494efad54c320179df1192001
Jedrzej <tcankon@gmail.com>
2026-04-13 20:23:42 +0200

    task 3

--- lista4/src/main/java/com/piisw/jpa/repositories/EventRepository.java
+++ lista4/src/main/java/com/piisw/jpa/repositories/EventRepository.java
@@ -4,6 +4,10 @@ import com.piisw.jpa.entities.Event;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.time.LocalDateTime;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
@@ -12,5 +16,21 @@ public interface EventRepository extends JpaRepository<Event, Long> {
             LocalDateTime end,
             boolean toBeAnalyzed,
             Pageable pageable
+    );
+
+    @Modifying
+    @Query("DELETE FROM Event e WHERE e.time < :threshold")
+    void deleteByTimeBefore(@Param("threshold") LocalDateTime threshold);
+
+    @Modifying
+    @Query("""
+    UPDATE Event e
+    SET e.analysisRequired = true
+    WHERE TYPE(e) = :clazz
+    AND e.duration > :minDuration
+""")
+    int updateAnalysisRequiredForSubclass(
+            @Param("clazz") Class<? extends Event> clazz,
+            @Param("minDuration") int minDuration
     );
 }
